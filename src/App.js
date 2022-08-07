60c074791ea049a217f3b65dd0d664eb23e44614
Michael Clautice <michaelclauticeusa@gmail.com>
2022-08-07 13:25:54 -0400

    S.8.7.22.1:25p

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// 🔴 Michael Clautice, Richmond, VA  
+// 🔴 React-JS-Weather-App
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🤠 HOWDY DOODY! 😎</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+// 🔴 Michael Clautice, Richmond, VA  
+// 🔴 React-JS-Weather-App
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
